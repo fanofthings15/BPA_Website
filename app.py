f07b890b5afe55b66cfdaf2aa283baa680a1a242
Isaac <54126919+Aizakkuno@users.noreply.github.com>
2023-11-01 09:20:26 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config["AUTO_RELOAD_TEMPLATES"] = True
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route("/")
 def view_index():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(port=8009)
+    app.run(host="0.0.0.0", port=8009)
