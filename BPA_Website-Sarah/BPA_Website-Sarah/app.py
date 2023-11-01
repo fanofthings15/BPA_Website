bf9d597a3667869fba9a580275ea8a5f30b602ae
SCPhillips06 <128173664+SCPhillips06@users.noreply.github.com>
2023-11-01 14:25:37 -0400

    Added new version of website
    There is now a txt for image credit links. Started making a slideshow for featured menu items.

---
+++ BPA_Website-Sarah/BPA_Website-Sarah/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route("/")
+def view_index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8009)
